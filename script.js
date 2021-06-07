6a274ece9c0835cbd4bdbe56bc67a60f9697e56b
峰 <leelapfung@gmail.com>
2021-06-07 10:10:14 +0800

    Add info

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ function play(speed) {
     x.play();
 
 }
+
+function openInfo() {
+    var closedDiv = document.getElementById("closed");
+    var openDiv = document.getElementById("open");
+    openDiv.style.display = "block";
+    closedDiv.style.display = "none";
+}
+
+function closeInfo() {
+    var closedDiv = document.getElementById("closed");
+    var openDiv = document.getElementById("open");
+    closedDiv.style.display = "block";
+    openDiv.style.display = "none";
+}
